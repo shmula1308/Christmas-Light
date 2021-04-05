ca4082ffff5c580c178ccfd3deb978a841b8f02c
Shpend Mula <shmula@hotmail.com>
2021-04-05 16:05:08 +0200

    javascript update

---
+++ js/script.js
@@ -0,0 +1,68 @@
+const play = document.querySelector(".on");
+const stop = document.querySelector(".off");
+const lights = document.getElementsByClassName("circle");
+const speedBtn = document.querySelector(".speed");
+const rowsBtn = document.querySelector(".row");
+const intensityBtn = document.querySelector(".intensity");
+const speedInput = document.querySelector(".input-speed");
+const rowsInput = document.querySelector(".input-rows");
+const lightBoard = document.querySelector(".light-board");
+
+
+play.addEventListener('click',on);
+stop.addEventListener('click',off);
+speedBtn.addEventListener('click', changeSpeed);
+rowsBtn.addEventListener('click', changeRows);
+
+
+
+
+function on() {
+  Array.from(lights).forEach(light => {
+      light.removeAttribute('style');
+      light.style.animationPlayState = "running";
+      light.style.backgroundColor = "none";
+  })
+}
+
+function off() {
+    Array.from(lights).forEach(light => {
+        light.style.animation = 'none';
+        light.style.backgroundColor = '#563260';
+    })
+}
+
+function changeSpeed() {
+    let speedValue = speedInput.value;
+    if(speedValue > 5) {
+        speedValue = 5;
+    }
+    Array.from(lights).forEach(light => {
+        light.style.animationDuration = speedValue +'s';
+    })
+}
+
+function changeRows() {
+   let rowValue = rowsInput.value;
+   let lightColorsRow = ['red','green','blue','yellow','red','green','blue','yellow'];
+   let df = new DocumentFragment();
+   for(let i = 0; i < rowsInput.value; i++) {
+     lightColorsRow.forEach(color => {
+        let div = document.createElement('div');
+        div.className = "circle";
+        div.className += " " + color;
+        df.append(div);
+     })
+   }
+   lightBoard.innerHTML = "";
+   lightBoard.append(df);
+}
+
+
+
+
+
+
+
+
+
